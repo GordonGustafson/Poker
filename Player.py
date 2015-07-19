7cfaac216bfb6f36fdce02b94909a40870e272d3
Lukas Froehlich <lukas.froehlich@hulu.com>
2015-07-18 17:36:05 -0700

    Changed player id to name and added total bet this round field

--- Player.py
+++ Player.py
@@ -1,9 +1,10 @@
 
 
 class Player:
-    def __init__(self, id, url, money):
-        self.id = id
+    def __init__(self, name, url, money):
+        self.name = name
         self.endpoint = url  
         self.money = money
         self.has_folded = False
         self.hand = None
+        self.total_bet_this_round = 0
