d8ffce28c6d34ac5ce7689cbf191b3f4162a0744
Gordon Gustafson <gordon.gustafson@hulu.com>
2015-07-26 17:44:33 -0700

    Changed true to True in cards.py
    It was used in a code path that is never taken, so I didn't find it
    until now. This is why you unit test...

--- cards.py
+++ cards.py
@@ -57,7 +57,7 @@ class HandType():
 def all_same_value(lst):
     if not lst:
         # there are no elements in the list, so all elements are the same value
-        return true
+        return True
     # return whether the number of occurrences of the first element
     # are equal to the number of elements in the list.
     return lst.count(lst[0]) == len(lst)
